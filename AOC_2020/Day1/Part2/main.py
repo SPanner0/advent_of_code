5719a4a100cf839b535e728d46b84b9a552bfd15
Wolfus Boreas <silvallywish@gmail.com>
2021-02-02 14:15:50 +0800

    Resolved some issues

--- AOC_2020/Day1/Part2/main.py
+++ AOC_2020/Day1/Part2/main.py
@@ -6,11 +6,7 @@ with open("AOC_2020\\Day1\\Part2\\inputD1P2.txt", 'r') as f:
 
 def find_sum():
 
-<<<<<<< HEAD
     # Nested loop for for three sets to find 3 numbers that sum to 2020 and print the product
-=======
-
->>>>>>> 3a5268b712c19a4336be675ba9e9091bd32001fb
     for x in dt:
         for y in dt:
             for z in dt:
