3a5268b712c19a4336be675ba9e9091bd32001fb
Wolfus Boreas <silvallywish@gmail.com>
2021-02-02 14:00:07 +0800

    Did some clean-up

--- AOC_2020/Day1/Part2/main.py
+++ AOC_2020/Day1/Part2/main.py
@@ -1,16 +1,22 @@
 import math
 
-file = open('inputD1P2.txt')
-nums = file.read().replace("\n", " ").split()
-file.close()
+with open("AOC_2020\\Day1\\Part2\\inputD1P2.txt", 'r') as f:
+    dt = f.read().replace("\n", " ").split()
 
-while sum != 2020:
-    for x in nums:
-        for y in nums:
-                for z in nums:
-                    sum = int(x) + int(y) + int(z)    
 
-                    if sum == 2020:           
-                        print(x, "+", y, "+", z, "=", sum)
-                        print("Product =", int(x)*int(y)*int(z))
-                        break
+def find_sum():
+
+
+    for x in dt:
+        for y in dt:
+            for z in dt:
+                x = int(x)
+                y = int(y)
+                z = int(z)
+                sum = x + y + z
+
+                if sum == 2020:
+                    return f"The three numbers that sum to {sum} are: {x}, {y}, and {z}; and their product is {x * y * z}"
+
+
+print(find_sum())
